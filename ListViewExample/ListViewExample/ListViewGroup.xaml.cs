73d57ed7db2b7c0d6ab3cad28f4315c5c3dc2076
John <pierojbh@gmail.com>
2023-05-07 18:26:48 -0500

    first commit

---
+++ ListViewExample/ListViewExample/ListViewGroup.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewGroup : ContentPage
+    {
+        public ObservableCollection<Group> Groups { get; set; }
+        public ListViewGroup()
+        {
+            InitializeComponent();
+            Groups = new ObservableCollection<Group>
+        {
+            new Group("Alfa")
+            {
+                new Item { Title = "Chacha", Subtitle = "Pika" },
+                new Item { Title = "Chachu", Subtitle = "Trebol" },
+                new Item { Title = "Chicha", Subtitle = "Corazon" },
+            },
+            new Group("Beta")
+            {
+                new Item { Title = "Cuadrado", Subtitle = "Play" },
+                new Item { Title = "Triangulo", Subtitle = "Xbox" },
+                new Item { Title = "cruz", Subtitle = "Nintendo" },
+      
+            },
+            new Group("Omega")
+            {
+                new Item { Title = "Herencia", Subtitle = "Java" },
+                new Item { Title = "Encalpsulamiento", Subtitle = "Swift" },
+                new Item { Title = "Abstraccion", Subtitle = "c#" },
+         
+            },
+        };
+            GroupedView.ItemsSource = Groups;
+        }
+    }
+}
---
+++ ListViewExample/ListViewExample/ListViewSource.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewSource : ContentPage
+    {
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+        public ObservableCollection<Employee> Employees { get { return employees; } }
+        public ListViewSource()
+        {
+            InitializeComponent();
+            // ObservableCollection allows items to be added after ItemsSource
+            // is set and the UI will react to changes
+            EmployeeView.ItemsSource = employees;
+            employees.Add(new Employee { DisplayName = "Rob Finnerty" });
+            employees.Add(new Employee { DisplayName = "Bill Wrestler" });
+            employees.Add(new Employee { DisplayName = "Dr. Geri-Beth Hooper" });
+            employees.Add(new Employee { DisplayName = "Dr. Keith Joyce-Purdy" });
+            employees.Add(new Employee { DisplayName = "Sheri Spruce" });
+            employees.Add(new Employee { DisplayName = "Burt Indybrick" });
+        }
+    }
+}
